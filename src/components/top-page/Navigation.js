aa1afc085a7f6d879063a97c8c5adbfcd840cee5
Schynol-dev <kamil.schynol0dev@gmail.com>
2020-10-31 23:54:04 +0100

    commit2

---
+++ src/components/top-page/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navigation() {
+
+    return (
+        <nav>
+            <ul>
+                <li>Offer</li>
+                <li>Job</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
